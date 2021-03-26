bf2150fa41a5232cb4387f86335053f865d109cd
LaraKhil <56624634+LaraKhil@users.noreply.github.com>
2021-03-26 20:45:04 +0800

    1

--- script.js
+++ script.js
@@ -6,17 +6,27 @@ const btnCloseModal = document.querySelector(".close-modal");
 const formUser = document.querySelector("#form");
 const formModal = document.querySelector(".form-modal");
 const btnModalOk = document.querySelector(".btn-modal-ok");
-
-
-
-
-
+const imgCard = document.querySelector(".custom-card ");
 
 
 
 barsBtn.addEventListener("click", () => modalWindow.style.display = "flex");
 btnCloseModal.addEventListener("click", () => modalWindow.style.display = "none");
 btnModalOk.addEventListener("click", ()=> formModal.style.display = "none");
+
+
+
+const API_URL = "https://pixabay.com/api/?key=20832657-1f0f4cf4fbcb1f209669bce16&q=seasons&image_type=photo";
+console.log(API_URL);
+
+fetch(API_URL)
+.then((response) => response.json())
+.then((responded)=> {
+    $(".custom-card").html(`
+    <img class="img" src=${responded.imageURL} /> 
+    `)
+    console.log(responded)
+});
 
 
 
